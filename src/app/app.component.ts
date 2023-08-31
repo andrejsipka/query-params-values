de925fdf755e641b2f39cc9b660655b6f61ce8c6
Andrej Sipka <andrei.sipka@gmail.com>
2023-08-31 17:24:34 +0200

    new: init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, switchMap, interval, Observable } from 'rxjs';
+import { DataAccess } from './data-access.service';
+
+type Country = {
+  name: string,
+  value: number
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'query-params-values';
+export class AppComponent implements OnInit {
+  public title = 'Query Params playground';
+
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+  private dataAccess = inject(DataAccess);
+
+  private interval$: Observable<number> = interval(10 * 1000);
+  private countries$: Observable<Country[]> = this.dataAccess.request();
+
+  public contries: Country[] = [];
+
+  public ngOnInit(): void {
+    this.router.events.pipe(
+      filter((e) => e instanceof NavigationEnd),
+      switchMap(() => this.route.queryParams)
+    ).subscribe({
+      next: (params) => {
+        console.log(params);
+        this.getCountries();
+      }
+    })
+  }
+
+  private getCountries(): void {
+    this.countries$.subscribe({
+      next: (data) => {
+        console.log(data);
+      },
+      error: (err: Error) => {
+        console.log(err.message);
+      }
+    })
+  }
 }
---
+++ src/app/data-access.service.ts
@@ -0,0 +1,120 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+type Country = {
+  name: string,
+  value: number
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataAccess {
+  public data = [
+    {
+      "name": "Bermuda",
+      "value": 57493
+    },
+    {
+      "name": "Chad",
+      "value": 32289
+    },
+    {
+      "name": "Norway",
+      "value": 30054
+    },
+    {
+      "name": "Sao Tome and Principe",
+      "value": 17086
+    },
+    {
+      "name": "Falkland Islands (Malvinas)",
+      "value": 25076
+    },
+    {
+      "name": "Guernsey",
+      "value": 46918
+    },
+    {
+      "name": "Indonesia",
+      "value": 51086
+    },
+    {
+      "name": "Åland Islands",
+      "value": 55414
+    },
+    {
+      "name": "Sri Lanka",
+      "value": 55499
+    },
+    {
+      "name": "European Union",
+      "value": 12257
+    },
+    {
+      "name": "Ukraine",
+      "value": 51808
+    },
+    {
+      "name": "Afghanistan",
+      "value": 11024
+    },
+    {
+      "name": "Croatia",
+      "value": 20860
+    },
+    {
+      "name": "Latvia",
+      "value": 23011
+    },
+    {
+      "name": "Bangladesh",
+      "value": 47368
+    },
+    {
+      "name": "Belgium",
+      "value": 21105
+    },
+    {
+      "name": "Åland Islands",
+      "value": 13878
+    },
+    {
+      "name": "Lithuania",
+      "value": 17079
+    },
+    {
+      "name": "Canada",
+      "value": 54530
+    },
+    {
+      "name": "Italy",
+      "value": 48070
+    }
+  ];
+
+  constructor() { }
+
+  private getRandomObj(arr: any[]): Country {
+    const randomNum = Math.floor(Math.random() * (arr.length - 0));
+    return arr[randomNum];
+  }
+
+  private randomContries(data: Country[], number: number): Country[] {
+    let results: Country[] = [];
+
+    // Loop over n times and get random country
+    for(let i = 0; i < number; i++) {
+      results.push(this.getRandomObj(data));
+    }
+
+    return results;
+  }
+
+  public request(): Observable<Country[]> {
+    return of(this.data).pipe(
+      map((data) => this.randomContries(data, 7))
+    );
+  }
+}
