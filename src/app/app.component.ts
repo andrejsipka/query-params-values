380e5723528214f4620388d553b76f3ffb6fdfda
Andrej Sipka <andrei.sipka@gmail.com>
2023-08-31 21:18:07 +0200

    time and date picker has been added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { NgForm, FormBuilder, FormGroup, NonNullableFormBuilder } from '@angular/forms';
 import { filter, switchMap, interval, Observable } from 'rxjs';
 import { DataAccess } from './data-access.service';
 
@@ -19,13 +20,21 @@ export class AppComponent implements OnInit {
   private route = inject(ActivatedRoute);
   private router = inject(Router);
   private dataAccess = inject(DataAccess);
+  private fb = inject(NonNullableFormBuilder);
 
   private interval$: Observable<number> = interval(10 * 1000);
   private countries$: Observable<Country[]> = this.dataAccess.request();
 
   public contries: Country[] = [];
+  public form: FormGroup = this.fb.group({
+    time: [this.setTime()],
+    date: [this.setDate()]
+  });
 
   public ngOnInit(): void {
+    console.log(this.form.get('date')?.value);
+    console.log(this.form.get('time')?.value);
+
     this.router.events.pipe(
       filter((e) => e instanceof NavigationEnd),
       switchMap(() => this.route.queryParams)
@@ -47,4 +56,17 @@ export class AppComponent implements OnInit {
       }
     })
   }
+
+  public setTime(): string {
+   return `${new Date().getHours()}:${new Date().getMinutes()}`
+  };
+
+  public setDate(): string {
+    const currDate = new Date();
+    const year = currDate.getFullYear();
+    const month = String(currDate.getMonth() + 1).padStart(2, '0');
+    const day = String(currDate.getDate()).padStart(2, '0');
+
+    return [year, month, day].join('-');
+  };
 }
